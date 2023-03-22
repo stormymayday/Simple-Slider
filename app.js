42aae203ed3fe4194ca33b879df4416e355f1b4c
stormymayday <stormymayday7@gmail.com>
2023-03-21 21:15:29 -0600

    Selecting the '.next-btn'

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 // Selections
 const slides = document.querySelectorAll('.slide');
 const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+console.log(slides, prevBtn, nextBtn);
