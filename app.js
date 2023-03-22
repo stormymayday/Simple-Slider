34f52c9a7452a9e92ba6b98390c3d35b9f1ba332
stormymayday <stormymayday7@gmail.com>
2023-03-21 21:25:21 -0600

    Setting the 'left' property on a slide to index * 100

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ const slides = document.querySelectorAll('.slide');
 const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
 console.log(slides, prevBtn, nextBtn);
+
+slides.forEach(function (slide, index) {
+
+    // Setting the 'left' property to index * 100
+    slide.style.left = `${index * 100}%`;
+
+});
