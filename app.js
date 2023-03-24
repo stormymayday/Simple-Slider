344381554cbe955a47b8ce44326af97f9a385cec
stormymayday <stormymayday7@gmail.com>
2023-03-23 22:08:11 -0600

    Switch back to the original approach: Resetting the counter when it is equal to slides.lenght AND Setting it to slides.length - 1 when it is less than 0

--- app.js
+++ app.js
@@ -25,41 +25,42 @@ prevBtn.addEventListener('click', function () {
 
 function carousel() {
 
-    // Initial Logic: Resetting the counter
-    // if (counter === slides.length) {
-    //     counter = 0;
-    // }
-    // if (counter < 0) {
-    //     counter = slides.length - 1;
-    // }
+    // Resetting the counter when it is equal to slides.length
+    if (counter === slides.length) {
+        counter = 0;
+    }
+    // Setting the counter to slides.length -1 when it is less that 0
+    if (counter < 0) {
+        counter = slides.length - 1;
+    }
 
     // Alternative: Hiding and Showing buttons
     // Requires: Hiding 'prevBtn' initally
     // Next Button
-    if (counter < slides.length - 1) {
+    // if (counter < slides.length - 1) {
 
-        // Showing the 'nextBtn' when counter is less than slides.length - 1
-        nextBtn.style.display = 'block';
+    //     // Showing the 'nextBtn' when counter is less than slides.length - 1
+    //     nextBtn.style.display = 'block';
 
-    } else {
+    // } else {
 
-        // Hiding the 'nextBtn'
-        nextBtn.style.display = 'none';
+    //     // Hiding the 'nextBtn'
+    //     nextBtn.style.display = 'none';
 
-    }
+    // }
 
     // Prev Button
-    if (counter > 0) {
+    // if (counter > 0) {
 
-        // Displaying 'prevBtn'
-        prevBtn.style.display = 'block';
+    //     // Displaying 'prevBtn'
+    //     prevBtn.style.display = 'block';
 
-    } else {
+    // } else {
 
-        // Hiding 'prevBtn'
-        prevBtn.style.display = 'none';
+    //     // Hiding 'prevBtn'
+    //     prevBtn.style.display = 'none';
 
-    }
+    // }
 
     slides.forEach(function (slide) {
 
@@ -71,4 +72,4 @@ function carousel() {
 }
 
 // Hiding 'prevBtn' initally
-prevBtn.style.display = 'none';
+// prevBtn.style.display = 'none';
